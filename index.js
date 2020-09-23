306c76cabf967cd1462410aa7aec389ba875b2d3
dadou1985 <davidsimba1985@yahoo.com>
2020-09-23 11:44:05 +0200

    exos javascript

---
+++ index.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/**
+ * Exercice 1
+ */
+ // My digital School Forever
+
+// Exercice 2
+
+ /*My digital
+ school
+ forever*/
+
+// Exercice 3
+
+ var a = "42";
+ var b = 42;
+ var c = [42];
+ var d = true;
+ var e = null;
+ var f = undefined;
+ var g = new Date(42)
+
+// Exercice 4
+
+ var my42count = ["quarante-deux"];
+ var numberOfString = my42count[0].length;
+
+// Exercice 5
+
+// Exercice 6
+
+ var myArray42 = ["q","u","a","r","a","n","t","e","-","d","e","u","x"]
+
+ // Exercice 7
+
+ var myArrayLen = myArray42
